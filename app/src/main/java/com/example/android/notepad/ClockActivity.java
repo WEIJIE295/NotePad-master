a6e2ddc6e624526de5cf49963b0444d27b4b9caa
linylx <1075389505@qq.com>
2017-05-13 03:54:44 +0800

    1

---
+++ app/src/main/java/com/example/android/notepad/ClockActivity.java
@@ -0,0 +1,30 @@
+package com.example.android.notepad;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class ClockActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.noteslist_item);
+        //创建一个闹钟提醒的对话框,点击确定关闭铃声与页面
+        new AlertDialog.Builder(ClockActivity.this).setTitle("闹钟").setMessage("设定的时间到了哦！").show();
+        /**
+         *  延时启动
+         */
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(ClockActivity.this,NotesList.class);
+                startActivity(intent);
+                ClockActivity.this.finish();
+            }
+        }, 1500);
+
+    }
+}
